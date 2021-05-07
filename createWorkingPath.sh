bd771919e1cd39227079807bd3fc334bcc22de4c
Tenzu <huangxin.cliff@gmail.com>
2021-05-07 10:05:49 +0800

    Create createWorkingPath.sh

---
+++ createWorkingPath.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Please input user name:"
+read userName
+userPath="/data/$userName"
+sudo mkdir $userPath
+#userGroup=$(groups $userName)
+#sudo chown $userName:$userGroup $userPath
+sudo chown $userName:students $userPath
+#sudo chown $userName:profs $userPath
+sudo chmod 799 $userPath
