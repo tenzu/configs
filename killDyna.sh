4834cf093f02a7fa34917ce0584ff66356aed5d9
Tenzu <huangxin.cliff@gmail.com>
2021-06-17 11:46:22 +0800

    Update killDyna.sh

--- killDyna.sh
+++ killDyna.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Please input user name:"
-read userName
-ps -ef | grep dyna | grep $userName | grep -v grep | awk '{print "sudo kill "$2}' | sh
+#echo "Please input user name:"
+#read userName
+#ps -ef | grep dyna | grep $userName | grep -v grep | awk '{print "sudo kill "$2}' | sh
+ps -ef | grep $1 | grep -v grep | awk '{print "sudo kill "$2}' | sh
