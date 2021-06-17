18ebdb07135e9d7a8fbb21df078a58144744828f
Tenzu <huangxin.cliff@gmail.com>
2021-06-17 11:47:18 +0800

    Update createWorkingPath.sh

--- createWorkingPath.sh
+++ createWorkingPath.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-echo "Please input user name:"
-read userName
-userPath="/data/$userName"
-sudo mkdir $userPath
-#userGroup=$(groups $userName)
-#sudo chown $userName:$userGroup $userPath
-sudo chown $userName:students $userPath
-#sudo chown $userName:profs $userPath
+#echo "Please input user name:"
+#read userName
+#userPath="/data/$userName"
+#sudo mkdir $userPath
+##userGroup=$(groups $userName)
+##sudo chown $userName:$userGroup $userPath
+#sudo chown $userName:students $userPath
+#sudo chmod 700 $userPath
+
+userPath="/data/$1"
+sudo mkdir /data/$1
+sudo chown $1:students $userPath
 sudo chmod 700 $userPath
