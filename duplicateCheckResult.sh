648b8452053eb244eb9e8d4ad3865502e77e604e
Tenzu <huangxin.cliff@gmail.com>
2022-05-30 23:56:26 +0800

    Update duplicateCheckResult.sh

--- duplicateCheckResult.sh
+++ duplicateCheckResult.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-docxList=$(ls | grep docx | cut -c 1-10)
-pdfList=$(ls | grep pdf | cut -c 1-10)
-for i in $docxList; do
-    result=$(echo $pdfList | grep "$i")
-    if [ "$result" != "" ]; then
-        echo "$i is OK!"
-    else
-        echo "$i is NOT OK!!!"
-    fi
+dirs=$(ls -F | grep "/$")
+for i in $dirs; do
+	cd $i
+	echo $(pwd)
+	docxList=$(ls | grep docx | cut -c 1-10)
+	pdfList=$(ls | grep pdf | cut -c 1-10)
+	for i in $docxList; do
+	    result=$(echo $pdfList | grep "$i")
+	    if [ "$result" != "" ]; then
+	        echo "$i is OK!"
+	    else
+	        echo "$i is NOT OK!!!"
+	    fi
+	done
+	cd ..
 done
